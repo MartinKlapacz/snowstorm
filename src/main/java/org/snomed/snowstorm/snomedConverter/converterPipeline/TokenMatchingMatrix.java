79dbbdf68c49f11ad7e40703132a771f9ef81943
Martin Klapacz <klapacz.martin@gmail.com>
2023-01-04 17:51:59 +0100

    use thread pool executor

--- src/main/java/org/snomed/snowstorm/snomedConverter/converterPipeline/TokenMatchingMatrix.java
+++ src/main/java/org/snomed/snowstorm/snomedConverter/converterPipeline/TokenMatchingMatrix.java
@@ -7,62 +7,86 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.*;
+import java.util.concurrent.*;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 @Service
 public class TokenMatchingMatrix {
-    private final Map<List<String>, Set<DIdContainer>> cells = new HashMap<>();
-
-    @Getter
-    private List<DIdContainer> topMatchingDescriptions = new ArrayList<>();
+    private final Map<List<String>, Set<DIdContainer>> cells = new ConcurrentHashMap<>();
     @Autowired
     AugmentedLexiconService augmentedLexiconService;
-
     @Autowired
     ConceptRepository conceptRepository;
-
     @Autowired
     DescriptionController descriptionController;
+    ExecutorService executorService = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+    @Getter
+    private List<DIdContainer> topMatchingDescriptions = new ArrayList<>();
 
     public void generateLexicon(List<String> tokens) {
-        Map<String, Set<DIdContainer>> tokenToDescriptionIdsMap = new HashMap<>();
+        Map<String, Set<DIdContainer>> tokenToDescriptionIdsMap = new ConcurrentHashMap<>();
+
+        int numOfCells = tokens.size() + tokens.size() * (tokens.size() - 1) / 2;
+        List<Future<?>> futureList = new ArrayList<>(numOfCells);
+
         // for each token get the descriptionIds/wordCounts of the descriptions that contain the token
         for (String token : tokens) {
-            Set<DIdContainer> conceptSet = augmentedLexiconService.getConceptsContainingToken(token);
-            tokenToDescriptionIdsMap.put(token, conceptSet);
-            cells.put(List.of(token), conceptSet);
+            Future<?> future = executorService.submit(() -> {
+                Set<DIdContainer> conceptSet = augmentedLexiconService.getConceptsContainingToken(token);
+                tokenToDescriptionIdsMap.put(token, conceptSet);
+                cells.put(List.of(token), conceptSet);
+            });
+            futureList.add(future);
         }
+        joinAndClearFutures(futureList);
 
 
         for (int i = 0; i < tokens.size(); i++) {
             for (int j = i + 1; j < tokens.size(); j++) {
-                // get current token subsequence from indices
-                List<String> tokenSubsequence = Arrays.stream(IntStream.rangeClosed(i, j).toArray())
-                        .boxed()
-                        .map(tokens::get)
-                        .collect(Collectors.toList());
+                int finalI = i;
+                int finalJ = j;
+                Future<?> future = executorService.submit(() -> {
+                    // get current token subsequence from indices
+                    List<String> tokenSubsequence = Arrays.stream(IntStream.rangeClosed(finalI, finalJ).toArray())
+                            .boxed()
+                            .map(tokens::get)
+                            .collect(Collectors.toList());
 
-                // get description Id set for each token in token subsequence
-                List<Set<DIdContainer>> dIdAndSizeSetsToIntersect = tokenSubsequence.stream()
-                        .map(tokenToDescriptionIdsMap::get)
-                        .collect(Collectors.toList());
+                    // get description Id set for each token in token subsequence
+                    List<Set<DIdContainer>> dIdAndSizeSetsToIntersect = tokenSubsequence.stream()
+                            .map(tokenToDescriptionIdsMap::get)
+                            .collect(Collectors.toList());
 
-                // compute their intersection
-                Set<DIdContainer> intersectedDIdAndSizeSet = new HashSet<>(dIdAndSizeSetsToIntersect.get(0));
-                dIdAndSizeSetsToIntersect.stream().skip(1).forEach(intersectedDIdAndSizeSet::retainAll);
+                    // compute their intersection
+                    Set<DIdContainer> intersectedDIdAndSizeSet = new HashSet<>(dIdAndSizeSetsToIntersect.get(0));
+                    dIdAndSizeSetsToIntersect.stream().skip(1).forEach(intersectedDIdAndSizeSet::retainAll);
 
 
-                // use the description size to compute the id scores
-                int tokenSubsequenceLength = String.join(" ", tokenSubsequence).length();
-                Set<DIdContainer> intersectedCIDsWithScores = intersectedDIdAndSizeSet.stream()
-                        .peek(dIdContainer -> dIdContainer.computeScore(tokenSubsequenceLength))
-                        .collect(Collectors.toSet());
+                    // use the description size to compute the id scores
+                    int tokenSubsequenceLength = String.join(" ", tokenSubsequence).length();
+                    Set<DIdContainer> intersectedCIDsWithScores = intersectedDIdAndSizeSet.stream()
+                            .peek(dIdContainer -> dIdContainer.computeScore(tokenSubsequenceLength))
+                            .collect(Collectors.toSet());
 
-                // store matrix cells
-                cells.put(tokenSubsequence, intersectedCIDsWithScores);
+                    // store matrix cells
+                    cells.put(tokenSubsequence, intersectedCIDsWithScores);
+                });
+                futureList.add(future);
             }
         }
+        joinAndClearFutures(futureList);
+    }
+
+    private void joinAndClearFutures(List<Future<?>> futureList){
+        futureList.forEach(future -> {
+            try {
+                future.get();
+            } catch (InterruptedException | ExecutionException e) {
+                throw new RuntimeException(e);
+            }
+        });
+        futureList.clear();
     }
 
     public void collectTopMatchingDescriptionIds(double threshold) {
