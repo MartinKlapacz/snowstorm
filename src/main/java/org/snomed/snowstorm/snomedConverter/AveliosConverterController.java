a0823566770171514cae59ba98552d005fa8ca49
Martin Klapacz <klapacz.martin@gmail.com>
2023-03-11 20:21:48 +0100

    fix

--- src/main/java/org/snomed/snowstorm/snomedConverter/AveliosConverterController.java
+++ src/main/java/org/snomed/snowstorm/snomedConverter/AveliosConverterController.java
@@ -66,7 +66,7 @@ public class AveliosConverterController {
 
         branchController.unlockBranch("MAIN");
         for (String branchId: idsOfBranchesToDelete){
-            Set<String> conceptIdsToDelete = snowstormSearchService.findConceptDescendants(branchId);
+            Set<String> conceptIdsToDelete = snowstormSearchService.findConceptDescendants(Set.of(branchId));
             conceptIdsToDelete.add(branchId);
             conceptService.deleteConceptAndComponents(conceptIdsToDelete, "MAIN", true);
         }
