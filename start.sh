8044b6e6e2415a9d1c278b54a223a718db2254b2
Martin Klapacz <klapacz.martin@gmail.com>
2023-04-10 10:07:25 +0200

    small changes

--- start.sh
+++ start.sh
@@ -1,1 +1,7 @@
-/home/martin/.jdks/corretto-17.0.6/bin/java --add-opens java.base/java.util=ALL-UNNAMED -Dfile.encoding=UTF-8 -jar /home/martin/pro/avelios/backend/snowstorm/target/snowstorm-7.11.0.jar --elasticsearch.urls=http://localhost:9200
+#!/bin/bash
+
+/home/martin/.jdks/corretto-17.0.6/bin/java \
+  --add-opens java.base/java.util=ALL-UNNAMED \
+  -Dfile.encoding=UTF-8 \
+  -jar /home/martin/pro/avelios/backend/snowstorm/target/snowstorm-7.11.0.jar \
+  --elasticsearch.urls=http://localhost:9200
